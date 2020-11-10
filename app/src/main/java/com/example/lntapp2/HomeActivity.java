f73b9eea3f4ac1ee5cedb7cb4394a3e22c6f4531
105051 <vodelashashank1@gmail.com>
2020-11-10 10:33:12 +0530

    first commit

---
+++ app/src/main/java/com/example/lntapp2/HomeActivity.java
@@ -0,0 +1,32 @@
+package com.example.lntapp2;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.util.Log;
+import android.widget.TextView;
+
+public class HomeActivity extends AppCompatActivity {
+
+    private static final String TAG = HomeActivity.class.getSimpleName();
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_home);
+        Log.w(TAG,"onCreate");
+
+        Intent intent = getIntent();
+
+        Bundle extras = intent.getExtras();
+
+        String data = extras.getString("srisai");
+
+        TextView textView = findViewById(R.id.textView);
+
+        textView.setText(data);
+
+    }
+
+}
---
+++ app/src/main/java/com/example/lntapp2/MainActivity.java
@@ -0,0 +1,30 @@
+package com.example.lntapp2;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+
+public class MainActivity extends AppCompatActivity {
+
+    private static final String TAG = MainActivity.class.getSimpleName();
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Log.i(TAG,"onCreate");
+    }
+
+    public void clickHandler(View view) {
+        Log.e(TAG,"clickHandler");
+        Intent hIntent = new Intent( MainActivity.this,HomeActivity.class);
+        hIntent.putExtra("srisai","chintu");
+        startActivity(hIntent);
+    }
+
+    public void clickHandler2(View view) {
+    }
+}
