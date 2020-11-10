a932eafe6d0e0aac9233e59268a6f93536a07aab
99002762 <Srisai.S@ltts.com>
2020-11-10 17:00:49 +0530

    Adapters and list views

--- app/src/main/java/com/example/lntapp2/HomeActivity.java
+++ app/src/main/java/com/example/lntapp2/HomeActivity.java
@@ -5,9 +5,13 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 import android.widget.TextView;
 
 public class HomeActivity extends AppCompatActivity {
+
+    String[] languages;
 
     private static final String TAG = HomeActivity.class.getSimpleName();
 
@@ -16,18 +20,24 @@ public class HomeActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
         Log.w(TAG,"onCreate");
-
+        languages = new String[]{"english","hindi","telugu","kannada"};
         Intent intent = getIntent();
 
         Bundle extras = intent.getExtras();
+         if(extras!= null) {
+             String data = extras.getString("srisai", "chinnu");
 
-        String data = extras.getString("srisai","chinnu");
-
-        TextView textView = findViewById(R.id.textView);
-        TextView textView2 = findViewById(R.id.textView2);
-        String data2 = "hello everyone this is my appli";
-        textView.setText(data);
-        textView2.setText(data2);
+             TextView textView = findViewById(R.id.textView);
+             TextView textView2 = findViewById(R.id.textView2);
+             String data2 = "hello everyone this is my appli";
+             textView.setText(data);
+             textView2.setText(data2);
+         }
+        ListView countriesListView = findViewById(R.id.countriesListview);
+        ArrayAdapter<String> adapter = new ArrayAdapter<>(this,
+                android.R.layout.simple_list_item_1, //layout file of each row in the listview
+                languages);
+        countriesListView.setAdapter(adapter);
     }
     @Override
     protected void onStart() {
